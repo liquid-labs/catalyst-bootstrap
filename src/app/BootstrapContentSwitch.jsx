329d854a05dae2ce5b7c08eae2754e0432c6bc08
Zane Rockenbaugh <zane@liquid-labs.com>
2019-01-04 20:52:02 -0600

    implement four-oh-four page; update catalyst packages

--- src/app/BootstrapContentSwitch.jsx
+++ src/app/BootstrapContentSwitch.jsx
@@ -3,15 +3,20 @@ import React from 'react'
 import { Route, Switch, withRouter } from 'react-router-dom'
 
 import { Dashboard } from '../components/app/ui/Dashboard'
+import { FourOhFour } from '../components/app/ui/FourOhFour'
 
 import * as bootstrapRoutes from './bootstrapRoutes'
 
-const BootstrapContentSwitchBase = ({location}) =>
+const BootstrapContentSwitchBase = ({location}) => {
+  console.log("location: ", location)
+  return (
   <Switch>
     <Route exact path={bootstrapRoutes.LANDING} component={() => <Dashboard />} />
     { /* default 404/Not Found handler */ }
-    { /* <Route component={() => <FourOhFour noAuthRedirect={unoRoutes.LANDING} />} />  */ }
+    <Route component={() => <FourOhFour noAuthRedirect={bootstrapRoutes.LANDING} />} />
   </Switch>
+)
+}
 
 // 'withRouter' makes the switch 'location' aware so it'll update when the URL
 // changes
---
+++ src/components/app/ui/FourOhFour.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { AccountControl } from '@liquid-labs/catalyst-users-ui'
+import { BasicContentFrame } from '@liquid-labs/catalyst-core-ui'
+import { FourOhFour as FourOhFourContent } from '@liquid-labs/catalyst-core-ui'
+
+const FourOhFour = (FourOhFourContentProps) => {
+  const accountControl = <AccountControl />
+
+  return (
+    <BasicContentFrame AppNavigationProps={{logoTo: '/', rightChildren:  accountControl }}>
+      <FourOhFourContent {...FourOhFourContentProps} />
+    </BasicContentFrame>
+  )
+}
+
+export { FourOhFour }
