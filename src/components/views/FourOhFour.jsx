c3b509e4def5e247d1856676dd5e5f4848f28955
Zane Rockenbaugh <zane@liquid-labs.com>
2019-04-17 10:05:57 -0500

    remove refence to AccountWidget

--- src/components/views/FourOhFour.jsx
+++ src/components/views/FourOhFour.jsx
@@ -2,14 +2,9 @@ import React from 'react'
 
 import { BasicContentFrame, FourOhFour as FourOhFourContent } from '@liquid-labs/catalyst-core-ui'
 
-const FourOhFour = (FourOhFourContentProps) => {
-  const accountControlWidget = <AccountControlWidget />
-
-  return (
-    <BasicContentFrame>
-      <FourOhFourContent {...FourOhFourContentProps} />
-    </BasicContentFrame>
-  )
-}
+const FourOhFour = (FourOhFourContentProps) =>
+  <BasicContentFrame>
+    <FourOhFourContent {...FourOhFourContentProps} />
+  </BasicContentFrame>
 
 export { FourOhFour }
