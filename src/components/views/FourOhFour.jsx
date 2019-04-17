2a1d3e893a10fb9cdeef55493b317b2bc607355d
Zane Rockenbaugh <zane@liquid-labs.com>
2019-04-17 10:02:57 -0500

    removed unused component import

--- src/components/views/FourOhFour.jsx
+++ src/components/views/FourOhFour.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import { AccountControlWidget } from '@liquid-labs/catalyst-users-ui'
 import { BasicContentFrame, FourOhFour as FourOhFourContent } from '@liquid-labs/catalyst-core-ui'
 
 const FourOhFour = (FourOhFourContentProps) => {
